4b534a1062fb569f0c9b26815a9151f209ebd57e
mershark <mersharktakyi@gmail.com>
2023-10-24 07:51:43 +0000

    Create menu.rb

---
+++ classes/item.rb
@@ -0,0 +1,32 @@
+class Item
+  attr_accessor :genre, :author, :label, :source, :publish_date
+  attr_reader :id, :archived
+
+  @count = 1
+
+  def initialize(date, archived: false)
+    @id = self.class.next_id
+    @publish_date = date
+    @archived = archived
+    @genre = nil
+    @author = nil
+    @label = nil
+    @source = nil
+  end
+
+  def self.next_id
+    @count ||= 1
+    @count += 1
+    @count - 1
+  end
+
+  def can_be_archived?
+    (Time.now.year - @publish_date.year) > 10
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
+end
