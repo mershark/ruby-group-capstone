1afb7702e4334c9acdd95d8a7bdcd1957f934838
prg-04 <karanjaevans95@gmail.com>
2023-10-24 15:34:02 +0300

    Modify Item class

--- classes/item.rb
+++ classes/item.rb
@@ -2,7 +2,7 @@ require 'date'
 
 class Item
   attr_accessor :genre, :author, :label, :source, :publish_date
-  attr_reader :id, :archived
+ 
 
   def initialize(genre, author, label, source, publish_date)
     @genre = genre
@@ -14,14 +14,19 @@ class Item
     @id = Random.rand(1..1000)
   end
 
-  def can_be_archived?
-    publish_date = Date.parse(@publish_date)
-    (Date.today.year - publish_date.year) > 10
-  end
 
-  def move_to_archive
+   def move_to_archive
     return unless can_be_archived?
 
     @archived = true
   end
+
+  private
+
+  def can_be_archived?
+    publish_date = Date.parse(@publish_date)
+    (Date.today.year - publish_date.year) > 10
+  end 
 end
+
+
