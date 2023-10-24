03eb2c5efbf0e8be1816b2eb375eb9ab74f4c721
Mike Dark <agyemankojo47@gmail.com>
2023-10-24 09:54:09 +0000

    Refactor main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,16 @@
+require './app'
+require_relative 'menu'
 
+response = App.new
 
+def prompt(response)
+  loop do
+    display_menu
+    option = gets.chomp.to_i
+    break if option.zero?
+
+    handle_option(option, response)
+  end
+end
+
+prompt(response)
