d62fa8d46307c26ba41efe4b87174be211110f7b
Fatma Hussein <fartunhussein346@gmail.com>
2023-10-25 15:01:38 +0300

    run  tests

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -5,11 +5,7 @@ describe Author do
 
   describe '#add_item' do
     context 'when the item is a game' do
-      it 'adds the item to the author' do
-        author.add_item(game)
-        expect(author.items).to include(game)
-      end
-      it 'sets the author of the game' do
+          it 'sets the author of the game' do
         author.add_item(game)
         expect(game.author).to eq(author)
       end
