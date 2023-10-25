4d00061d3beb26a6886a605a53630cb0254cf865
Fatma Hussein <fartunhussein346@gmail.com>
2023-10-25 15:16:13 +0300

    fix test errors

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,10 +2,11 @@ require_relative '../author'
 
 describe Author do
   let(:author) { Author.new('John', 'Doe') }
+  let(:game) { Game.new('2012-01-01', false, true, '2014-01-01', author) }
 
   describe '#add_item' do
     context 'when the item is a game' do
-          it 'sets the author of the game' do
+      it 'sets the author of the game' do
         author.add_item(game)
         expect(game.author).to eq(author)
       end
