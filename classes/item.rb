1c10fa91ae71833b1e0aa073ed05741728c0eab2
prg-04 <karanjaevans95@gmail.com>
2023-10-25 07:18:34 +0300

    Add source and movies class

--- classes/item.rb
+++ classes/item.rb
@@ -2,7 +2,6 @@ require 'date'
 
 class Item
   attr_accessor :genre, :author, :label, :source, :publish_date
-  attr_reader :id, :archived
 
   def initialize(genre, author, label, source, publish_date)
     @genre = genre
@@ -10,18 +9,20 @@ class Item
     @label = label
     @source = source
     @publish_date = publish_date
-    @archived = archived
+    @archived = false
     @id = Random.rand(1..1000)
-  end
-
-  def can_be_archived?
-    publish_date = Date.parse(@publish_date)
-    (Date.today.year - publish_date.year) > 10
   end
 
   def move_to_archive
     return unless can_be_archived?
 
     @archived = true
+  end
+
+  private
+
+  def can_be_archived?
+    publish_date = Date.parse(@publish_date)
+    (Date.today.year - publish_date.year) > 10
   end
 end
