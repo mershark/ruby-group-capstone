d963dd3a8622370af70fa5f5b4ae639d23f4b1d7
Fatma Hussein <fartunhussein346@gmail.com>
2023-10-25 14:58:29 +0300

    fix test errors

--- author.rb
+++ author.rb
@@ -1,12 +1,12 @@
 class Author
   attr_reader :id, :first_name, :last_name
-  attr_accessor :authors
+  attr_accessor :items
 
   def initialize(first_name, last_name)
     @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
-    @authors = []
+    @items = []
   end
 
   def add_item(item)
